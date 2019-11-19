8368604d7f4e81fa9151346b2ecec7d25bbbe6bc
Richard Buckle <github@sailmaker.co.uk>
2019-11-19 18:37:21 +0000

    Copy from sample project

--- AvalonCottle/MainWindow.xaml.cs
+++ AvalonCottle/MainWindow.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Search;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
 
 namespace AvalonCottle
 {
@@ -20,9 +17,102 @@ namespace AvalonCottle
     /// </summary>
     public partial class MainWindow : Window
     {
+        CompletionWindow completionWindow;
+        private string currentFileName;
+
         public MainWindow()
         {
+            // Load our custom highlighting definition
+            IHighlightingDefinition customHighlighting;
+            using (Stream s = typeof(MainWindow).Assembly.GetManifestResourceStream("AvalonCottle.Cottle.xshd"))
+            {
+                if (s == null)
+                {
+                    throw new InvalidOperationException("Could not find embedded resource");
+                }
+
+                using (XmlReader reader = new XmlTextReader(s))
+                {
+                    customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.
+                        HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+            // and register it in the HighlightingManager
+            HighlightingManager.Instance.RegisterHighlighting("Cottle", new string[] { ".cottle" }, customHighlighting);
+
             InitializeComponent();
+
+            this.SetValue(TextOptions.TextFormattingModeProperty, TextFormattingMode.Display);
+
+            textEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
+            textEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
+            SearchPanel.Install(textEditor);
+        }
+
+        void openFileClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                Filter = "Cottle files (*.cottle)|*.cottle|Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                CheckFileExists = true
+            };
+            if (dlg.ShowDialog() ?? false)
+            {
+                currentFileName = dlg.FileName;
+                textEditor.Load(currentFileName);
+                textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(Path.GetExtension(currentFileName));
+            }
+        }
+
+        void saveFileClick(object sender, EventArgs e)
+        {
+            if (currentFileName == null)
+            {
+                SaveFileDialog dlg = new SaveFileDialog {DefaultExt = ".cottle"};
+                if (dlg.ShowDialog() ?? false)
+                {
+                    currentFileName = dlg.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            textEditor.Save(currentFileName);
+        }
+
+        void textEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == ".")
+            {
+                // open code completion after the user has pressed dot:
+                completionWindow = new CompletionWindow(textEditor.TextArea);
+                // provide AvalonEdit with the data:
+                IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                data.Add(new MyCompletionData("Item1"));
+                data.Add(new MyCompletionData("Item2"));
+                data.Add(new MyCompletionData("Item3"));
+                data.Add(new MyCompletionData("Another item"));
+                completionWindow.Show();
+                completionWindow.Closed += delegate
+                {
+                    completionWindow = null;
+                };
+            }
+        }
+
+        void textEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // do not set e.Handled=true - we still want to insert the character that was typed
         }
     }
 }
---
+++ AvalonCottle/MyCompletionData.cs
@@ -0,0 +1,43 @@
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using System;
+
+namespace AvalonCottle
+{
+    /// <summary>
+    /// Implements AvalonEdit ICompletionData interface to provide the entries in the completion drop down.
+    /// </summary>
+    public class MyCompletionData : ICompletionData
+    {
+        public MyCompletionData(string text)
+        {
+            this.Text = text;
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text { get; private set; }
+
+        // Use this property if you want to show a fancy UIElement in the drop down list.
+        public object Content
+        {
+            get { return this.Text; }
+        }
+
+        public object Description
+        {
+            get { return "Description for " + this.Text; }
+        }
+
+        public double Priority { get { return 0; } }
+
+        public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, this.Text);
+        }
+    }
+}
